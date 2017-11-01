6e8392b599427c2ad9bc7a8e775fc5213705d918
karlfrazier <lordkthesith@gmail.com>
2017-10-31 20:44:57 -0700

    Initial commit

---
+++ api.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
